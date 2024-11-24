8929ca292063f9e55d8c7e499c6382ded4746bff
Serdar Geldyev <serdarabad@gmail.com>
2024-11-24 10:21:39 +0300

    Updates

---
+++ js/0.bundle.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/scss/swiper-bundle.min.css":
+/*!****************************************!*\
+  !*** ./src/scss/swiper-bundle.min.css ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.bundle.js.map

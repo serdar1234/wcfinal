5db605aa23683b041bc6794bb375df85816eaef3
Serdar Geldyev <serdarabad@gmail.com>
2024-11-24 20:55:38 +0300

    sidebar is working

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,19 +1,19 @@
-const burgerBtn = document.querySelector('.nav__link--open-menu');
-const xBurgerBtn = document.querySelector('.nav__link--close-menu');
-const sideBar = document.querySelector('.sidebar');
-const fogOfWar = document.querySelector('.fog-of-war');
-const swiper = document.querySelector('.swiper');
+const burgerBtn = document.querySelector('.nav__link--open-menu')
+const xBurgerBtn = document.querySelector('.nav__link--close-menu')
+const sideBar = document.querySelector('.sidebar')
+const fogOfWar = document.querySelector('.fog-of-war')
+const swiper = document.querySelector('.swiper')
 
 function toggleBurgerBtn() {
-  sideBar.classList.toggle('sidebar--hidden');
-    fogOfWar.classList.toggle('active');
-    swiper.classList.toggle('swiper--hidden');
+  sideBar.classList.toggle('sidebar--hidden')
+  fogOfWar.classList.toggle('active')
+  swiper.classList.toggle('swiper--hidden')
 }
 
 function burgerFunction() {
-  burgerBtn.addEventListener('click', toggleBurgerBtn);
-  sideBar.addEventListener('click', toggleBurgerBtn);
-  fogOfWar.addEventListener('click', toggleBurgerBtn);
+  burgerBtn.addEventListener('click', toggleBurgerBtn)
+  xBurgerBtn.addEventListener('click', toggleBurgerBtn)
+  fogOfWar.addEventListener('click', toggleBurgerBtn)
 }
 
 export { burgerFunction }

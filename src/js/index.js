d04a661b775b31b8a67bb4bf5c7bf4f51a5490f4
Serdar Geldyev <serdarabad@gmail.com>
2024-11-24 12:18:21 +0300

    swiper pagination improved

--- src/js/index.js
+++ src/js/index.js
@@ -30,40 +30,34 @@ function initializeSwiper() {
   const mediaQuery = window.matchMedia('(max-width:767.98px)').matches
 
   if (mediaQuery) {
-    swiper = new Swiper('.swiper', {
+    swiper = new Swiper(".swiper", {
       // Optional parameters
-      slidesPerView: 'auto',
+      slidesPerView: "auto",
       loop: true,
       spaceBetween: 16,
       // For accessibility
       keyboard: {
         enabled: true,
-        onlyInViewport: false
-      },
-      // Correcting pagination
-      hashNavigation: {
-        watchState: true
+        onlyInViewport: false,
       },
       pagination: {
-        el: '.swiper-pagination'
-      }
-    })
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        dynamicMainBullets: 8,
+      },
+    });
   }
 }
 
 initializeSwiper()
 
-// Adding data-hash attr to correct pagination
-allListItems.forEach((slide, index) => {
-  slide.setAttribute('data-hash', `slide${index + 1}`)
-})
-
-window.addEventListener('resize', function () {
+window.addEventListener("resize", function () {
   if (window.innerWidth >= 768 && swiper !== null) {
     // delete swiper
-    swiper.destroy()
-  } else {
+    swiper.destroy();
+    swiper = null;
+  } else if (window.innerWidth < 768 && swiper === null) {
     // initialize swiper again
-    initializeSwiper()
+    initializeSwiper();
   }
-})
+});

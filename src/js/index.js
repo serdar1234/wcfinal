ba1ba3f4401b570595e3053250acc964d64c96d0
Serdar Geldyev <serdarabad@gmail.com>
2024-11-25 23:14:03 +0300

    minor improvements

--- src/js/index.js
+++ src/js/index.js
@@ -5,38 +5,16 @@ import { toggleBrandsFunction } from './brands.js';
 import { changeActiveLink } from './sidebarLinks.js';
 import { changeActiveBtn } from './sidebarBtns.js';
 import { changeActiveScrollLink } from './scrollMenuLinks.js'
+import { initializeSwiper } from './swiperFunction.js';
 
 brands__button.addEventListener('click', toggleBrandsFunction);
-
 burgerFunction();
 changeActiveLink();
 changeActiveBtn();
 changeActiveScrollLink();
+
 // **************** Swiper *************
 let swiper = null
-
-export function initializeSwiper() {
-  const mediaQuery = window.matchMedia('(max-width:767.98px)').matches
-
-  if (mediaQuery) {
-    swiper = new Swiper(".swiper", {
-      // Optional parameters
-      slidesPerView: "auto",
-      loop: true,
-      spaceBetween: 16,
-      // For accessibility
-      keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-        dynamicMainBullets: 8,
-      },
-    });
-  }
-}
 
 initializeSwiper();
 
